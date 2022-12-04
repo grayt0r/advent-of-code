4e814b0dceb6f9fbbe49d7887b17a934022405f0
Ross Grayton <rossgrayton@gmail.com>
2022-12-04 10:29:32 -0500

    Day 4, part 2

---
+++ 2022/4/part2.mjs
@@ -0,0 +1,38 @@
+import { readFileSync } from "node:fs";
+
+function run() {
+  try {
+    // const data = readFileSync("./example.txt", { encoding: "utf8" });
+    const data = readFileSync("./input.txt", { encoding: "utf8" });
+
+    const lines = data.split("\n");
+
+    function buildAssignmentsAsSet(start, end) {
+      return new Set(
+        Array.from({ length: end - start + 1 }, (v, i) => i + start)
+      );
+    }
+
+    function isOverlap(s1, s2) {
+      const intersection = [...s1].filter((z) => s2.has(z));
+      return intersection.length > 0;
+    }
+
+    const result = lines
+      .map((l) =>
+        l
+          .split(",")
+          .map((x) => x.split("-").map((y) => parseInt(y, 10)))
+          .map(([a, b]) => buildAssignmentsAsSet(a, b))
+      )
+      .filter(([a, b]) => isOverlap(a, b)).length;
+
+    console.log(result);
+
+    // CORRECT ANSWER: 933
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run();
