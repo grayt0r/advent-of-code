c07bf3dcc0197722399d7ca72199c8b10239989e
Ross Grayton <rossgrayton@gmail.com>
2022-12-08 10:53:41 -0500

    Day 8, part 1

---
+++ 2022/8/part1.mjs
@@ -0,0 +1,72 @@
+import { readFileSync } from "node:fs";
+
+function buildArray(start, end) {
+  return Array.from({ length: end - start }, (_, i) => start + i);
+}
+
+function getHeightsAbove(grid, x, y) {
+  return buildArray(0, y)
+    .map((y2) => grid[y2][x])
+    .reverse();
+}
+
+function getHeightsBelow(grid, x, y) {
+  return buildArray(y + 1, grid.length).map((y2) => grid[y2][x]);
+}
+
+function getHeightsLeft(grid, x, y) {
+  return buildArray(0, x)
+    .map((x2) => grid[y][x2])
+    .reverse();
+}
+
+function getHeightsRight(grid, x, y) {
+  return buildArray(x + 1, grid[0].length).map((x2) => grid[y][x2]);
+}
+
+function calculateMinHeightToEdge(grid, x, y) {
+  return Math.min(
+    Math.max(...getHeightsAbove(grid, x, y)),
+    Math.max(...getHeightsBelow(grid, x, y)),
+    Math.max(...getHeightsLeft(grid, x, y)),
+    Math.max(...getHeightsRight(grid, x, y))
+  );
+}
+
+function run() {
+  try {
+    // const data = readFileSync("./example.txt", { encoding: "utf8" });
+    const data = readFileSync("./input.txt", { encoding: "utf8" });
+
+    const grid = data
+      .split("\n")
+      .map((x) => x.split("").map((y) => parseInt(y, 10)));
+
+    let interiorTreesVisible = 0;
+
+    for (let y = 1; y + 1 < grid.length; y++) {
+      const row = grid[y];
+
+      for (let x = 1; x + 1 < row.length; x++) {
+        const height = grid[y][x];
+        const minHeightToEdge = calculateMinHeightToEdge(grid, x, y);
+
+        if (height > minHeightToEdge) {
+          interiorTreesVisible++;
+        }
+      }
+    }
+
+    const exteriorTreesVisible = grid.length * 2 + (grid[0].length - 2) * 2;
+
+    const result = exteriorTreesVisible + interiorTreesVisible;
+
+    console.log(result);
+
+    // CORRECT ANSWER: 1823
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run();
