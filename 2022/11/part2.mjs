c4a4d10aa445d7ff3664891dbe6eecef1e057064
Ross Grayton <rossgrayton@gmail.com>
2022-12-11 23:34:39 -0500

    Day 11, part 2

---
+++ 2022/11/part2.mjs
@@ -0,0 +1,97 @@
+import { readFileSync } from "node:fs";
+
+function parseMonkeys(data) {
+  return data.split("\n\n").map((x) => {
+    const [
+      ,
+      itemsLine,
+      operationLine,
+      divisibleByLine,
+      trueOutcomeLine,
+      falseOutcomeLine,
+    ] = x.split("\n").map((y) => y.split(":")[1].trim());
+
+    const items = itemsLine.split(",").map((y) => parseInt(y.trim(), 10));
+    const [op, opArg] = operationLine
+      .match(/new = old (\*|\+) (old|\d+)/)
+      .slice(1);
+    const [divisibleBy] = divisibleByLine
+      .match(/divisible by (\d+)/)
+      .slice(1)
+      .map((y) => parseInt(y, 10));
+    const [trueOutcome] = trueOutcomeLine
+      .match(/throw to monkey (\d+)/)
+      .slice(1)
+      .map((y) => parseInt(y, 10));
+    const [falseOutcome] = falseOutcomeLine
+      .match(/throw to monkey (\d+)/)
+      .slice(1)
+      .map((y) => parseInt(y, 10));
+
+    return {
+      items,
+      op,
+      opArg: opArg !== "old" ? parseInt(opArg, 10) : undefined,
+      divisibleBy,
+      trueOutcome,
+      falseOutcome,
+      inspectionCount: 0,
+    };
+  });
+}
+
+function calculateWorryLevel(item, op, opArg) {
+  const arg = opArg ?? item;
+
+  switch (op) {
+    case "*":
+      return item * arg;
+    case "+":
+      return item + arg;
+    default:
+      throw new Error(`Unexpected op: ${op}`);
+  }
+}
+
+function run() {
+  try {
+    // const data = readFileSync("./example.txt", { encoding: "utf8" });
+    const data = readFileSync("./input.txt", { encoding: "utf8" });
+
+    const monkeys = parseMonkeys(data);
+
+    // TODO: read around least common multiple / modular arithmetic
+    const LCM = monkeys.map((m) => m.divisibleBy).reduce((a, b) => a * b);
+
+    for (let i = 0; i < 10_000; i++) {
+      monkeys.forEach((m) => {
+        m.items.forEach((item) => {
+          const updatedItem = calculateWorryLevel(item, m.op, m.opArg) % LCM;
+
+          const monkeyIndex =
+            updatedItem % m.divisibleBy === 0 ? m.trueOutcome : m.falseOutcome;
+          monkeys[monkeyIndex].items.push(updatedItem);
+
+          m.inspectionCount++;
+        });
+
+        m.items = [];
+      });
+    }
+
+    const inspectionCounts = monkeys
+      .map((x) => x.inspectionCount)
+      .sort((a, b) => b - a);
+
+    const result = inspectionCounts[0] * inspectionCounts[1];
+
+    console.log(result);
+
+    // CORRECT EXAMPLE ANSWER: 2713310158
+    // CORRECT ANSWER: 18170818354
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run();
