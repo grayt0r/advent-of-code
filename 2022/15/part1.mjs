bab22241de17a097b04aed22b04165e0c31b82b4
Ross Grayton <rossgrayton@gmail.com>
2022-12-16 11:43:46 -0500

    Optimise part 1
    Previous solution would be far too slow for part 2.

--- 2022/15/part1.mjs
+++ 2022/15/part1.mjs
@@ -14,15 +14,15 @@ function parseInput(data) {
 
     const sensor = [sensorX, sensorY];
     const closestBeacon = [beaconX, beaconY];
-    const distance = calculateManhattanDistance(sensor, closestBeacon);
+    const distanceToBeacon = calculateManhattanDistance(sensor, closestBeacon);
 
-    minX = Math.min(minX, sensorX - distance);
-    maxX = Math.max(maxX, sensorX + distance);
+    minX = Math.min(minX, sensorX - distanceToBeacon);
+    maxX = Math.max(maxX, sensorX + distanceToBeacon);
 
     return {
       sensor,
       closestBeacon,
-      distance,
+      distanceToBeacon,
     };
   });
 
@@ -33,47 +33,74 @@ function calculateManhattanDistance([x1, y1], [x2, y2]) {
   return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 }
 
+function calculateRanges(input, y, minX, maxX) {
+  const ranges = [];
+
+  input.forEach(({ sensor: [sensorX, sensorY], distanceToBeacon }) => {
+    const distanceToLine = Math.abs(sensorY - y);
+
+    if (distanceToLine <= distanceToBeacon) {
+      const offset = distanceToBeacon - distanceToLine;
+      const start = sensorX - offset;
+      const end = sensorX + offset;
+
+      if (start <= maxX && end >= minX) {
+        ranges.push([Math.max(start, minX), Math.min(end, maxX)]);
+      }
+    }
+  });
+
+  ranges.sort(([x1, y1], [x2, y2]) => (x1 === x2 ? y1 - y2 : x1 - x2));
+
+  const combinedRanges = [];
+
+  for (let i = 0; i < ranges.length; i++) {
+    if (!combinedRanges.length) {
+      combinedRanges.push(ranges[i]);
+    } else {
+      const [start, end] = ranges[i];
+      const [prevStart, prevEnd] = combinedRanges[combinedRanges.length - 1];
+
+      if (start <= prevEnd) {
+        combinedRanges[combinedRanges.length - 1] = [
+          prevStart,
+          Math.max(end, prevEnd),
+        ];
+      } else {
+        combinedRanges.push(ranges[i]);
+      }
+    }
+  }
+
+  return combinedRanges;
+}
+
 function run() {
   try {
     // const data = readFileSync("./example.txt", { encoding: "utf8" });
     const data = readFileSync("./input.txt", { encoding: "utf8" });
 
     // example
-    // const y = 10;
+    // const Y_LINE = 10;
     // input
-    const y = 2_000_000;
+    const Y_LINE = 2_000_000;
 
     const [input, minX, maxX] = parseInput(data);
 
-    // console.log("minX:", minX);
-    // console.log("maxX:", maxX);
-    // console.log("input:", input);
+    // console.log("minX", minX);
+    // console.log("maxX", maxX);
 
     console.time("part1");
 
-    let result = 0;
+    const ranges = calculateRanges(input, Y_LINE, minX, maxX);
 
-    for (let x = minX; x <= maxX; x++) {
-      const cannotBeBeacon = input.some(
-        ({ sensor, closestBeacon: [beaconX, beaconY], distance }) => {
-          if (x === beaconX && y === beaconY) {
-            return false;
-          }
+    let result = ranges.reduce((t, [s, e]) => t + (e - s), 0);
 
-          return calculateManhattanDistance(sensor, [x, y]) <= distance;
-        }
-      );
-
-      if (cannotBeBeacon) {
-        result++;
-      }
-    }
-
-    // TODO: improve perf
     console.timeEnd("part1");
 
     console.log(result);
 
+    // CORRECT EXAMPLE ANSWER: 26
     // CORRECT ANSWER: 4424278
   } catch (err) {
     console.error(err);
