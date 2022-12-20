9e601387dc22505c65d012dbb7e3d6981d524728
Ross Grayton <rossgrayton@gmail.com>
2022-12-20 10:44:54 -0500

    Day 17, part 2

--- 2022/17/part2.mjs
+++ 2022/17/part2.mjs
@@ -52,18 +52,10 @@ const shapes = [
   },
 ];
 
-function coordsToKey(x, y) {
-  return `${x},${y}`;
-}
-
 function calculateNewCoords([cx, cy], [ox, oy], shape, board) {
   const newCoords = shape.rock.map(([rx, ry]) => [cx + rx + ox, cy + ry + oy]);
 
-  if (
-    newCoords.some(
-      ([x, y]) => board.has(coordsToKey(x, y)) || x < 0 || x >= MAX_WIDTH
-    )
-  ) {
+  if (newCoords.some(([x, y]) => x < 0 || x >= MAX_WIDTH || board[x][y])) {
     return;
   }
 
@@ -74,43 +66,90 @@ function isMovePossible(coords, offsets, shape, board) {
   return !!calculateNewCoords(coords, offsets, shape, board);
 }
 
+function generateState(shapeIndex, blastIndex, highestY, board) {
+  const endY = Math.max(highestY - 100, 0);
+
+  let state = `${shapeIndex}|${blastIndex}|`;
+
+  for (let i = highestY; i > endY; i--) {
+    for (let j = 0; j < MAX_WIDTH; j++) {
+      state += board[j][i] ? "#" : ".";
+    }
+  }
+
+  return state;
+}
+
 function run() {
   try {
     // const data = readFileSync("./example.txt", { encoding: "utf8" });
     const data = readFileSync("./input.txt", { encoding: "utf8" });
 
+    console.time("time");
+
     const blasts = data.split("");
-    const board = new Set(
-      Array.from({ length: MAX_WIDTH }, (_, i) => [i, 0]).map(([x, y]) =>
-        coordsToKey(x, y)
-      )
-    );
+    const board = Array.from({ length: MAX_WIDTH }, () => [true]);
 
     let highestY = 0;
     let shapeCount = 0;
     let shape;
     let currentCoords;
+    let tick;
 
-    const NUM_ROCKS = 2_022;
+    let pastStates = new Map();
+    let result;
 
-    for (let tick = 0; shapeCount < NUM_ROCKS; tick++) {
+    const NUM_ROCKS = 1_000_000_000_000;
+
+    for (tick = 0; shapeCount < NUM_ROCKS; tick++) {
       if (!shape) {
-        shape = shapes[shapeCount % shapes.length];
+        const shapeIndex = shapeCount % shapes.length;
+
+        shape = shapes[shapeIndex];
         currentCoords = [2, highestY + 4];
+
+        const state = generateState(
+          shapeIndex,
+          tick % blasts.length,
+          highestY,
+          board
+        );
+
+        const matchingState = pastStates.get(state);
+
+        // if we have found a repeating pattern then we can calculate the result
+        if (matchingState) {
+          const totalRepeatingRocks = NUM_ROCKS - matchingState.shapeCount;
+          const shapeCountDiff = shapeCount - matchingState.shapeCount;
+          const highestYDiff = highestY - matchingState.highestY;
+
+          const repeatCount = totalRepeatingRocks / shapeCountDiff;
+          const remainder = totalRepeatingRocks % shapeCountDiff;
+
+          if (remainder === 0) {
+            result = matchingState.highestY + highestYDiff * repeatCount;
+            break;
+          }
+        } else {
+          // otherwise save the state so we can check it later
+          pastStates.set(state, { tick, highestY, shapeCount });
+        }
       }
 
       const blastOffset = blasts[tick % blasts.length] === "<" ? -1 : 1;
 
+      // attempt to move horizontally for blast
       if (isMovePossible(currentCoords, [blastOffset, 0], shape, board)) {
         currentCoords = [currentCoords[0] + blastOffset, currentCoords[1]];
       }
 
+      // attempt to move down
       if (isMovePossible(currentCoords, [0, -1], shape, board)) {
         currentCoords = [currentCoords[0], currentCoords[1] - 1];
       } else {
         calculateNewCoords(currentCoords, [0, 0], shape, board).forEach(
           ([x, y]) => {
-            board.add(coordsToKey(x, y));
+            board[x][y] = true;
 
             highestY = Math.max(highestY, y);
           }
@@ -121,12 +160,12 @@ function run() {
       }
     }
 
-    const result = highestY;
+    console.timeEnd("time");
 
     console.log(result);
 
-    // CORRECT EXAMPLE ANSWER: ?
-    // CORRECT ANSWER: ?
+    // CORRECT EXAMPLE ANSWER: 1514285714288
+    // CORRECT ANSWER: 1581449275319
   } catch (err) {
     console.error(err);
   }
