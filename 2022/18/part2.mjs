51f357e9be1b997b912578602a1642dec8416df8
Ross Grayton <rossgrayton@gmail.com>
2022-12-21 05:35:20 -0500

    Day 18, part 1

---
+++ 2022/18/part2.mjs
@@ -0,0 +1,58 @@
+import { readFileSync } from "node:fs";
+
+function coordsToKey(x, y, z) {
+  return `${x},${y},${z}`;
+}
+
+function keyToCoords(key) {
+  return key.split(",").map((x) => parseInt(x, 10));
+}
+
+function getNeighbourCoords([x, y, z]) {
+  return [
+    [x - 1, y, z],
+    [x + 1, y, z],
+    [x, y - 1, z],
+    [x, y + 1, z],
+    [x, y, z - 1],
+    [x, y, z + 1],
+  ];
+}
+
+function countOpenSides(cube, cubes) {
+  return getNeighbourCoords(keyToCoords(cube)).reduce((total, [x, y, z]) => {
+    if (!cubes.has(coordsToKey(x, y, z))) {
+      total++;
+    }
+
+    return total;
+  }, 0);
+}
+
+function run() {
+  try {
+    // const data = readFileSync("./example.txt", { encoding: "utf8" });
+    const data = readFileSync("./input.txt", { encoding: "utf8" });
+
+    console.time("time");
+
+    const input = data.split("\n");
+
+    const cubes = new Set(input);
+
+    const result = [...cubes]
+      .map((c) => countOpenSides(c, cubes))
+      .reduce((a, b) => a + b, 0);
+
+    console.timeEnd("time");
+
+    console.log(result);
+
+    // CORRECT EXAMPLE ANSWER: ?
+    // CORRECT ANSWER: ?
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run();
