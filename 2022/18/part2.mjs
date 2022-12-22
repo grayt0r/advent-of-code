27924afe96cc4c6c19349eae58f982c9dd039f81
Ross Grayton <rossgrayton@gmail.com>
2022-12-22 06:42:55 -0500

    Calculate limits programmatically

--- 2022/18/part2.mjs
+++ 2022/18/part2.mjs
@@ -8,6 +8,37 @@ function keyToCoords(key) {
   return key.split(",").map((x) => parseInt(x, 10));
 }
 
+function calculateAir(input, cubes) {
+  const all = input.map((x) => keyToCoords(x));
+
+  const allX = all.map(([x]) => x);
+  const allY = all.map(([, y]) => y);
+  const allZ = all.map(([, , z]) => z);
+
+  const minX = Math.min(...allX) - 1;
+  const maxX = Math.max(...allX) + 1;
+  const minY = Math.min(...allY) - 1;
+  const maxY = Math.max(...allY) + 1;
+  const minZ = Math.min(...allZ) - 1;
+  const maxZ = Math.max(...allZ) + 1;
+
+  const air = new Set();
+
+  for (let x = minX; x <= maxX; x++) {
+    for (let y = minY; y <= maxY; y++) {
+      for (let z = minZ; z <= maxZ; z++) {
+        const key = coordsToKey(x, y, z);
+
+        if (!cubes.has(key)) {
+          air.add(key);
+        }
+      }
+    }
+  }
+
+  return air;
+}
+
 function getNeighbourCoords([x, y, z]) {
   return [
     [x - 1, y, z],
@@ -54,41 +85,12 @@ function run() {
     // const data = readFileSync("./example.txt", { encoding: "utf8" });
     const data = readFileSync("./input.txt", { encoding: "utf8" });
 
-    // TODO: calculate these limits programmatically
-    // example
-    // const X_START = 0;
-    // const X_END = 4;
-    // const Y_START = 0;
-    // const Y_END = 4;
-    // const Z_START = 0;
-    // const Z_END = 7;
-    // input
-    const X_START = -1;
-    const X_END = 21;
-    const Y_START = -1;
-    const Y_END = 22;
-    const Z_START = -1;
-    const Z_END = 22;
-
     console.time("time");
 
     const input = data.split("\n");
 
     const cubes = new Set(input);
-    const air = new Set();
-
-    for (let x = X_START; x <= X_END; x++) {
-      for (let y = Y_START; y <= Y_END; y++) {
-        for (let z = Z_START; z <= Z_END; z++) {
-          const key = coordsToKey(x, y, z);
-
-          if (!cubes.has(key)) {
-            air.add(key);
-          }
-        }
-      }
-    }
-
+    const air = calculateAir(input, cubes);
     const outerAir = walk(coordsToKey(0, 0, 0), air);
 
     const result = [...cubes]
