3073571107650d7e558e2f1ed6d59471fe2dc6db
Ross Grayton <rossgrayton@gmail.com>
2022-12-22 05:34:37 -0500

    Day 18, part 2

--- 2022/18/part2.mjs
+++ 2022/18/part2.mjs
@@ -20,8 +20,8 @@ function getNeighbourCoords([x, y, z]) {
 }
 
 function countOpenSides(cube, cubes) {
-  return getNeighbourCoords(keyToCoords(cube)).reduce((total, [x, y, z]) => {
-    if (!cubes.has(coordsToKey(x, y, z))) {
+  return getNeighbourCoords(keyToCoords(cube)).reduce((total, coords) => {
+    if (!cubes.has(coordsToKey(...coords))) {
       total++;
     }
 
@@ -29,27 +29,86 @@ function countOpenSides(cube, cubes) {
   }, 0);
 }
 
+function walk(start, cubes) {
+  const queue = [start];
+  const seen = new Set([start]);
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+
+    getNeighbourCoords(keyToCoords(current)).forEach((c) => {
+      const key = coordsToKey(...c);
+
+      if (cubes.has(key) && !seen.has(key)) {
+        queue.push(key);
+        seen.add(key);
+      }
+    });
+  }
+
+  return seen;
+}
+
 function run() {
   try {
     // const data = readFileSync("./example.txt", { encoding: "utf8" });
     const data = readFileSync("./input.txt", { encoding: "utf8" });
+
+    // TODO: calculate these limits programmatically
+    // example
+    // const X_START = 0;
+    // const X_END = 4;
+    // const Y_START = 0;
+    // const Y_END = 4;
+    // const Z_START = 0;
+    // const Z_END = 7;
+    // input
+    const X_START = -1;
+    const X_END = 21;
+    const Y_START = -1;
+    const Y_END = 22;
+    const Z_START = -1;
+    const Z_END = 22;
 
     console.time("time");
 
     const input = data.split("\n");
 
     const cubes = new Set(input);
+    const air = new Set();
 
-    const result = [...cubes]
+    for (let x = X_START; x <= X_END; x++) {
+      for (let y = Y_START; y <= Y_END; y++) {
+        for (let z = Z_START; z <= Z_END; z++) {
+          const key = coordsToKey(x, y, z);
+
+          if (!cubes.has(key)) {
+            air.add(key);
+          }
+        }
+      }
+    }
+
+    const outerAir = walk(coordsToKey(0, 0, 0), air);
+
+    const innerAir = new Set([...air].filter((a) => !outerAir.has(a)));
+
+    const totalArea = [...cubes]
       .map((c) => countOpenSides(c, cubes))
       .reduce((a, b) => a + b, 0);
+
+    const innerArea = [...innerAir]
+      .map((c) => countOpenSides(c, innerAir))
+      .reduce((a, b) => a + b, 0);
+
+    const result = totalArea - innerArea;
 
     console.timeEnd("time");
 
     console.log(result);
 
-    // CORRECT EXAMPLE ANSWER: ?
-    // CORRECT ANSWER: ?
+    // CORRECT EXAMPLE ANSWER: 58
+    // CORRECT ANSWER: 2576
   } catch (err) {
     console.error(err);
   }
